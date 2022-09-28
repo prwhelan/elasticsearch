e3f86aa0d155cdacf7fa365fe759c3fe80e04b79
Mark Vieira <portugee@gmail.com>
2022-09-28 14:58:38 -0700

    Add stateless skeleton project

---
+++ x-pack/plugin/stateless/build.gradle.kts
@@ -0,0 +1,5 @@
+esplugin {
+    name = "stateless"
+    description = "Test plugin for statless distribution"
+    classname = "co.elastic.elasticsearch.stateless.StatelessPlugin"
+}
---
+++ x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/StatelessPlugin.java
@@ -0,0 +1,6 @@
+package co.elastic.elasticsearch.stateless;
+
+import org.elasticsearch.plugins.Plugin;
+
+public class StatelessPlugin extends Plugin {
+}

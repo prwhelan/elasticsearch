7be5634f0446e38d343df9e35cc9fc802ddc9062
Tanguy Leroux <tlrx.dev@gmail.com>
2022-10-18 17:14:22 +0200

    Introduce feature flag and setting in stateless

--- x-pack/plugin/stateless/build.gradle.kts
+++ x-pack/plugin/stateless/build.gradle.kts
@@ -1,5 +1,27 @@
+import org.elasticsearch.gradle.internal.info.BuildParams
+
+plugins {
+    id("elasticsearch.internal-cluster-test")
+}
+
 esplugin {
     name = "stateless"
-    description = "Test plugin for statless distribution"
-    classname = "co.elastic.elasticsearch.stateless.StatelessPlugin"
+    description = "Stateless module for Elasticsearch"
+    classname = "co.elastic.elasticsearch.stateless.Stateless"
+}
+
+dependencies {
+    compileOnly("org.elasticsearch.plugin:core")
+    internalClusterTestImplementation("org.elasticsearch.plugin:core") {
+        capabilities {
+            requireCapabilities("org.elasticsearch.gradle:core-test-artifacts")
+        }
+    }
+}
+
+tasks {
+    internalClusterTest {
+        systemProperty("es.use_stateless", "true")
+        enabled = BuildParams.isSnapshotBuild()
+    }
 }

66e033d4fd8673de2400387fecf15d16ea51be70
Pat Whelan <pat.whelan@elastic.co>
2025-09-24 16:15:28 -0400

    Revert "[Transform] Fix Missing Config Errors (#134963)"
    This reverts commit 8a75ecd12eab7e026f469a358ca660c05fc09ee5.

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/transform/action/GetTransformActionRequestTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/transform/action/GetTransformActionRequestTests.java
@@ -7,37 +7,28 @@
 
 package org.elasticsearch.xpack.core.transform.action;
 
-import org.elasticsearch.TransportVersion;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.core.TimeValue;
-import org.elasticsearch.xpack.core.ml.AbstractBWCWireSerializationTestCase;
+import org.elasticsearch.test.AbstractWireSerializingTestCase;
 import org.elasticsearch.xpack.core.transform.action.GetTransformAction.Request;
 
-import static org.elasticsearch.xpack.core.transform.action.GetTransformAction.DANGLING_TASKS;
-
-public class GetTransformActionRequestTests extends AbstractBWCWireSerializationTestCase<Request> {
+public class GetTransformActionRequestTests extends AbstractWireSerializingTestCase<Request> {
 
     @Override
     protected Request createTestInstance() {
         if (randomBoolean()) {
             return new Request(Metadata.ALL);
         }
-        return new Request(randomAlphaOfLengthBetween(1, 20), randomBoolean(), randomPositiveTimeValue());
+        return new Request(randomAlphaOfLengthBetween(1, 20));
     }
 
     @Override
     protected Request mutateInstance(Request instance) {
-        return randomValueOtherThan(instance, this::createTestInstance);
+        return null;// TODO implement https://github.com/elastic/elasticsearch/issues/25929
     }
 
     @Override
     protected Writeable.Reader<Request> instanceReader() {
         return Request::new;
-    }
-
-    @Override
-    protected Request mutateInstanceForVersion(Request instance, TransportVersion version) {
-        return version.onOrAfter(DANGLING_TASKS) ? instance : new Request(instance.getId(), true, TimeValue.MAX_VALUE);
     }
 }
--- x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/rest/action/RestGetTransformAction.java
+++ x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/rest/action/RestGetTransformAction.java
@@ -7,7 +7,6 @@
 
 package org.elasticsearch.xpack.transform.rest.action;
 
-import org.elasticsearch.action.support.master.AcknowledgedRequest;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
@@ -40,11 +39,10 @@ public class RestGetTransformAction extends BaseRestHandler {
 
     @Override
     protected RestChannelConsumer prepareRequest(RestRequest restRequest, NodeClient client) {
-        var id = restRequest.param(TransformField.ID.getPreferredName());
-        var checkForDanglingTasks = restRequest.paramAsBoolean(TransformField.CHECK_FOR_DANGLING_TASKS.getPreferredName(), true);
-        var timeout = restRequest.paramAsTime(TransformField.TIMEOUT.getPreferredName(), AcknowledgedRequest.DEFAULT_ACK_TIMEOUT);
+        GetTransformAction.Request request = new GetTransformAction.Request();
 
-        var request = new GetTransformAction.Request(id, checkForDanglingTasks, timeout);
+        String id = restRequest.param(TransformField.ID.getPreferredName());
+        request.setResourceId(id);
         request.setAllowNoResources(restRequest.paramAsBoolean(ALLOW_NO_MATCH.getPreferredName(), true));
         if (restRequest.hasParam(PageParams.FROM.getPreferredName()) || restRequest.hasParam(PageParams.SIZE.getPreferredName())) {
             request.setPageParams(
